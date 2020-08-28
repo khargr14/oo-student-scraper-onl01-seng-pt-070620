e5eee5813c5b4fad3e6f5cf4620896f90e47a7c7
Keesha Hargrove <kharg14@gmail.com>
2020-08-28 00:10:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,17 +1,4 @@
-# require 'open-uri'
-# require 'pry'
 
-# class Scraper
-
-#   def self.scrape_index_page(index_url)
-    
-#   end
-
-#   def self.scrape_profile_page(profile_url)
-    
-#   end
-
-# end
 
 require 'open-uri'
 require 'pry'
